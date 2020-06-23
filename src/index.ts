941e8ff272ce6291bfad272b7ef0ad9f70c3851e
8cy <50817549+8cy@users.noreply.github.com>
2020-06-23 03:04:28 -0700

    mega big changes but epic :star: 0.0.4
    # .prettierrc.yml :8ball:
    - change `tabwidth` to 4 instead of 2
    
    # package.json :+1:
    - change version from 0.0.3 to 0.0.4
    - change description :star2:
    - make clean script use rimraf instead of `rm -rf` for multi-platform compatibility.
    - remove lint function from prepack script
    - change homepage and repo links
    - change author to sin
    
    # src/index.ts :hotsprings:
    - change line ending format from crlf to lf *dont remember, might be vice versa*
    - change file indentation from 2 space to 1, 4 space tab
    - change `clientId` to valid one
    - change change state value in activity from the `workspace.root` to the `workspace.workspaceFolder.name` so the rpc doesnt show the whole path, just the folder name
    - change state value output from "On:" to "Workspace:"
    - add a large image + large image hover text
    
    # yarn.lock :cat:
    - auto generated

--- src/index.ts
+++ src/index.ts
@@ -3,46 +3,49 @@ import { Client } from 'discord-rpc';
 import * as O from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
 
-const clientId = '694558978776105000';
+const clientId = '724898679726538824';
 
 const setActivity = (client: Client, startTimestamp: number) => {
-  const details = pipe(
-    O.fromNullable(workspace.uri),
-    O.filter((x) => x.startsWith('file:///')),
-    O.map((x) => x.substr(8)),
-    O.map((x) => x.split('/')),
-    O.filter((xs) => xs.length > 0),
-    O.map((xs) => xs.reverse()[0]),
-    O.map((x) => `Editing ${x}`),
-    O.toUndefined,
-  );
+	const details = pipe(
+	O.fromNullable(workspace.uri),
+		O.filter((x) => x.startsWith('file:///')),
+		O.map((x) => x.substr(8)),
+		O.map((x) => x.split('/')),
+		O.filter((xs) => xs.length > 0),
+		O.map((xs) => xs.reverse()[0]),
+		O.map((x) => `Editing ${x}`),
+		O.toUndefined,
+	);
+	
+	const state = pipe(
+		O.fromNullable(workspace.workspaceFolder.name),
+        O.map((x) => x.split('/')),
+        O.filter((xs) => xs.length > 0),
+        O.map((xs) => xs.reverse()[0]),
+        O.map((x) => `Workspace: ${x}`),
+        O.toUndefined,
+    );
 
-  const state = pipe(
-    O.fromNullable(workspace.root),
-    O.map((x) => x.split('/')),
-    O.filter((xs) => xs.length > 0),
-    O.map((xs) => xs.reverse()[0]),
-    O.map((x) => `On ${x}`),
-    O.toUndefined,
-  );
+    const largeImageKey = 'neovim';
+    const largeImageText = 'Neovim';
 
-  client.setActivity({ state, details, startTimestamp, instance: false });
+  	client.setActivity({ state, details, startTimestamp, instance: false, largeImageKey, largeImageText });
 };
 
 const activate = () => {
-  const discordRpcClient = new Client({ transport: 'ipc' });
+  	const discordRpcClient = new Client({ transport: 'ipc' });
 
-  discordRpcClient.connect(clientId);
-  // eslint-disable-next-line no-console
-  discordRpcClient.login({ clientId }).catch(() => console.warn('Could not connect coc-discord client to Discord.'));
-  // TODO Add output channel
+	discordRpcClient.connect(clientId);
+	// eslint-disable-next-line no-console
+	discordRpcClient.login({ clientId }).catch(() => console.warn('Could not connect coc-discord client to Discord.'));
+	// TODO Add output channel
 
-  const startTimestamp = Date.now();
+	const startTimestamp = Date.now();
 
-  discordRpcClient.on('ready', () => {
-    setActivity(discordRpcClient, startTimestamp);
-    setInterval(() => setActivity(discordRpcClient, startTimestamp), 10000);
-  });
+	discordRpcClient.on('ready', () => {
+		setActivity(discordRpcClient, startTimestamp);
+		setInterval(() => setActivity(discordRpcClient, startTimestamp), 10000);
+	});
 };
 
 export { activate };
